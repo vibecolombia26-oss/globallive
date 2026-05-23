c5238f46e8a31bad50525a367839a45f502eaa0c
vibecolombia26-oss <vibecolombia26@gmail.com>
2026-05-23 16:53:36 -0500

    Login y panel admin corregidos
    Signed-off-by: vibecolombia26-oss <vibecolombia26@gmail.com>

--- src/main/java/com/globallive/globallive/AdminController.java
+++ src/main/java/com/globallive/globallive/AdminController.java
@@ -1,6 +1,5 @@
 package com.globallive.globallive;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
